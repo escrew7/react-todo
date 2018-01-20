f72aaf11bee346ea3b3a4b0737d6dc9f5b236c8a
Luke Rees <escrew7@hotmail.com>
2018-01-20 10:51:03 +0800

    add actions and action test

---
+++ app/tests/actions/actions.test.jsx
@@ -0,0 +1,42 @@
+var expect = require('expect')
+var actions = require('actions')
+
+describe('Actions', () => {
+  it('should genereate search text action', () => {
+    var action = {
+      type : 'SET_SEARCH_TEXT',
+      searchText : 'some search text'
+    }
+    var res = actions.setSearchText(action.searchText)
+
+    expect(res).toEqual(action)
+  })
+
+  it('should genereate addTodo actions', () => {
+    var action = {
+      type: 'ADD_TODO',
+      text: 'Thing to do'
+    }
+    var res = actions.addTodo(action.text)
+
+    expect(res).toEqual(action)
+  })
+
+  it('should show completed', () => {
+    var action = {
+      type: 'TOGGLE_SHOW_COMPLETED'
+    }
+    var res = actions.showCompleted()
+
+    expect(res).toEqual(action)
+  })
+
+  it('should toggle todos', () => {
+    var action = {
+      type: 'TOGGLE_TODO',
+      id: 1234
+    }
+    var res = actions.toggleTodo(action.id)
+    expect(res).toEqual(action)
+  })
+})
