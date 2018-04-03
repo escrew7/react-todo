edca377a2bdcb72f9594f504fa9ed2eac58a9565
Luke Rees <escrew7@hotmail.com>
2018-04-03 09:46:42 +0800

    add react trouter middlewear and redirect on auth

--- app/app.jsx
+++ app/app.jsx
@@ -1,14 +1,20 @@
 var React = require('react');
 var {Provider} = require('react-redux')
 var ReactDOM = require('react-dom');
-var {Route, Router, IndexRoute, hashHistory} = require('react-router');
-
-import Login from 'Login'
-import TodoApp from 'TodoApp'
+var {hashHistory} = require('react-router');
 
 var actions = require('actions')
 var store = require('configureStore').configure()
-var TodoAPI = require('TodoAPI')
+import firebase from 'app/firebase/'
+import router from 'app/router/'
+
+firebase.auth().onAuthStateChanged((user) => {
+  if(user) {
+    hashHistory.push('/todoApp')
+  } else {
+    hashHistory.push('/')
+  }
+})
 
 store.dispatch(actions.startAddTodos())
 
@@ -18,15 +24,11 @@ $(document).foundation();
 //load css
 require('style!css!sass!applicationStyles')
 
+
+
 ReactDOM.render(
   <Provider store={store}>
-    <Router history = {hashHistory}>
-      <Route path="/">
-        <Route path="TodoApp" component={TodoApp}/>
-        <IndexRoute component={Login}/>
-      </Route>
-    </Router>
-
+    {router}
   </Provider>,
   document.getElementById('app')
 );
